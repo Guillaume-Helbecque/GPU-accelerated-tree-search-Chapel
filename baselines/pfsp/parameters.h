3c0d05d43a08571e7d27f542ea1210f4ce608949
Ivan Tagliaferro <ivantezoto@hotmail.com>
2024-05-30 16:26:31 +0200

    Solving merging issues on parameters file

--- baselines/pfsp/parameters.h
+++ baselines/pfsp/parameters.h
@@ -1,19 +1,11 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
-#include <unistd.h>
-#include <limits.h>
-#include <getopt.h>
-#include <time.h>
 
 #define BLOCK_SIZE 512
 
 #define MAX_JOBS 20
 
 #define MAX_MACHINES 20
-
-//#define MAX_SIZE (MAX_JOBS + 2)
 
 typedef struct
 {
