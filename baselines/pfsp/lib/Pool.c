fe33f07dfdff75c14b1f04e30f07032e5a40098a
Ivan Tagliaferro <ivantezoto@hotmail.com>
2024-06-13 18:40:04 +0200

    Doing 1st step BFS and 2nd and 3rd step DFS on pfsp_gpu_cuda.c. Adding popFront function to Pool library.

--- baselines/pfsp/lib/Pool.c
+++ baselines/pfsp/lib/Pool.c
@@ -5,24 +5,38 @@ void initSinglePool(SinglePool* pool)
 {
   pool->elements = (Node*)malloc(CAPACITY * sizeof(Node));
   pool->capacity = CAPACITY;
+  pool->front = 0;
   pool->size = 0;
 }
 
 void pushBack(SinglePool* pool, Node node)
 {
-  if (pool->size >= pool->capacity) {
+  if (pool->front + pool->size >= pool->capacity) {
     pool->capacity *= 2;
     pool->elements = (Node*)realloc(pool->elements, pool->capacity * sizeof(Node));
   }
 
-  pool->elements[pool->size++] = node;
+  pool->elements[pool->front + pool->size] = node;
+  pool->size += 1;
 }
 
 Node popBack(SinglePool* pool, int* hasWork)
 {
   if (pool->size > 0) {
     *hasWork = 1;
-    return pool->elements[--pool->size];
+    pool->size--;
+    return pool->elements[pool->front + pool->size];
+  }
+
+  return (Node){0};
+}
+
+Node popFront(SinglePool* pool, int* hasWork)
+{
+  if (pool->size > 0) {
+    *hasWork = 1;
+    pool->size--;
+    return pool->elements[pool->front++];
   }
 
   return (Node){0};
