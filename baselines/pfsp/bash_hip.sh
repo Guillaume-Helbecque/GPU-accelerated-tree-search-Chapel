141e9b49cc5ad05961bfda008a66330331952b64
Ivan Tagliaferro <ivantezoto@hotmail.com>
2024-06-19 16:09:25 +0200

    Adding bash file to compile hip version

--- baselines/pfsp/bash_hip.sh
+++ baselines/pfsp/bash_hip.sh
@@ -5,8 +5,8 @@ gcc -O3 -Wall -g -c -fopenmp lib/PFSP_node.c -o lib/PFSP_node.o -I/share/compile
 gcc -O3 -Wall -g -c -fopenmp lib/Auxiliary.c -o lib/Auxiliary.o -I/share/compilers/nvidia/cuda/12.0/include -I/usr/local/cuda-11.2/targets/x86_64-linux/include/
 gcc -O3 -Wall -g -c -fopenmp lib/Pool.c -o lib/Pool.o -I/share/compilers/nvidia/cuda/12.0/include -I/usr/local/cuda-11.2/targets/x86_64-linux/include/
 hipify-perl lib/evalute.cu > lib/evaluate.cu.hip
-hipcc -O3 -offload-arch=gfx906 lib/evaluate.cu.hip -o evaluate_hip.o
+hipcc -O3 -offload-arch=gfx906 lib/evaluate.cu.hip -o evaluate_hip.o -lm -L/opt/rocm-4.5.0/hip/lib
 hipify-perl lib/c_bounds_gpu.cu > lib/c_bounds_gpu.cu.hip
-hipcc -O3 -offload-arch=gfx906 lib/c_bounds_gpu.cu.hip -o c_bounds_gpu_hip.o
+hipcc -O3 -offload-arch=gfx906 lib/c_bounds_gpu.cu.hip -o c_bounds_gpu_hip.o -lm -L/opt/rocm-4.5.0/hip/lib
 hipify-perl pfsp_gpu_cuda.c > pfsp_gpu_hip.c.hip
-gcc -O3 -Wall -g -c -fopenmp pfsp_gpu_hip.c.hip -o pfsp_gpu_hip.o -I/share/compilers/nvidia/cuda/12.0/include -I/usr/local/cuda-11.2/targets/x86_64-linux/include/
+gcc -O3 -Wall -g -c -fopenmp pfsp_gpu_hip.c.hip -o pfsp_gpu_hip.o -I/share/compilers/nvidia/cuda/12.0/include -I/usr/local/cuda-11.2/targets/x86_64-linux/include/ -lm -L/opt/rocm-4.5.0/hip/lib
