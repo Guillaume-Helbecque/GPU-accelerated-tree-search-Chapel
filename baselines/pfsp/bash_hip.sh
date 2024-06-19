a744086ca5039bb43a3a282f434a774d61bb9f5d
Ivan Tagliaferro <ivantezoto@hotmail.com>
2024-06-19 22:34:34 +0200

    Working on bash file for compiling hip

--- baselines/pfsp/bash_hip.sh
+++ baselines/pfsp/bash_hip.sh
@@ -10,3 +10,4 @@ hipify-perl lib/c_bounds_gpu.cu > lib/c_bounds_gpu.cu.hip
 hipcc -O3 -offload-arch=gfx906 lib/c_bounds_gpu.cu.hip -o c_bounds_gpu_hip.o -lm -L/opt/rocm-4.5.0/hip/lib
 hipify-perl pfsp_gpu_cuda.c > pfsp_gpu_hip.c.hip
 gcc -O3 -Wall -g -c -fopenmp pfsp_gpu_hip.c.hip -o pfsp_gpu_hip.o -I/share/compilers/nvidia/cuda/12.0/include -I/usr/local/cuda-11.2/targets/x86_64-linux/include/ -lm -L/opt/rocm-4.5.0/hip/lib
+gcc -O3 -Wall -g pfsp_gpu_hip.o lib/c_taillard.o lib/c_bound_simple.o lib/c_bound_johnson.o lib/PFSP_node.o lib/Auxiliary.o lib/evaluate.o lib/c_bounds_gpu.o lib/Pool.o -o pfsp_gpu_hip.out -lm -lcudart -L/usr/local/cuda-11.2/targets/x86_64-linux/lib/
