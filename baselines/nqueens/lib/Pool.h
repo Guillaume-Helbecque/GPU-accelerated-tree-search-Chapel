205ba9eeb3e153bd7b4f44c1631251f3d09910a1
Guillaume-Helbecque <helbecque.guillaume@gmail.com>
2024-09-10 16:50:47 +0200

    centralise Pool implementation as well

---
+++ baselines/nqueens/lib/Pool.h
@@ -0,0 +1,41 @@
+#ifndef POOL_H
+#define POOL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "PFSP_node.h"
+
+/*******************************************************************************
+Implementation of a dynamic-sized single pool data structure.
+Its initial capacity is 1024, and we reallocate a new container with double
+the capacity when it is full. Since we perform only DFS, it only supports
+'pushBack' and 'popBack' operations.
+*******************************************************************************/
+
+#define CAPACITY 1024
+
+typedef struct
+{
+  Node* elements;
+  int capacity;
+  int front;
+  int size;
+} SinglePool;
+
+void initSinglePool(SinglePool* pool);
+
+void pushBack(SinglePool* pool, Node node);
+
+Node popBack(SinglePool* pool, int* hasWork);
+
+Node popFront(SinglePool* pool, int* hasWork);
+
+void deleteSinglePool(SinglePool* pool);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // POOL_H
--- baselines/nqueens/nqueens_c.c
+++ baselines/nqueens/nqueens_c.c
@@ -10,54 +10,7 @@
 #include <time.h>
 
 #include "lib/NQueens_node.h"
-
-/*******************************************************************************
-Implementation of a dynamic-sized single pool data structure.
-Its initial capacity is 1024, and we reallocate a new container with double
-the capacity when it is full. Since we perform only DFS, it only supports
-'pushBack' and 'popBack' operations.
-*******************************************************************************/
-
-#define CAPACITY 1024
-
-typedef struct
-{
-  Node* elements;
-  int capacity;
-  int size;
-} SinglePool;
-
-void initSinglePool(SinglePool* pool)
-{
-  pool->elements = (Node*)malloc(CAPACITY * sizeof(Node));
-  pool->capacity = CAPACITY;
-  pool->size = 0;
-}
-
-void pushBack(SinglePool* pool, Node node)
-{
-  if (pool->size >= pool->capacity) {
-    pool->capacity *= 2;
-    pool->elements = (Node*)realloc(pool->elements, pool->capacity * sizeof(Node));
-  }
-
-  pool->elements[pool->size++] = node;
-}
-
-Node popBack(SinglePool* pool, int* hasWork)
-{
-  if (pool->size > 0) {
-    *hasWork = 1;
-    return pool->elements[--pool->size];
-  }
-
-  return (Node){0};
-}
-
-void deleteSinglePool(SinglePool* pool)
-{
-  free(pool->elements);
-}
+#include "lib/Pool.h"
 
 /*******************************************************************************
 Implementation of the sequential N-Queens search.
